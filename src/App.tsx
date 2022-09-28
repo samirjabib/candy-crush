550d3a18725a0ca8516332a6db351c98c307add9
samirjabib <samir.jc123@gmail.com>
2022-09-28 12:11:55 -0500

    column popping 3 & 4

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { useAppDispatch } from "./hooks/use-app-dispatch";
 import { useAppSelector } from "./hooks/use-app-selector"
 import { updateBoard } from "./store/slices/candy-crush/candy-crush.slice";
 import { createBoard } from "./utils/create-board";
+import { formulaForColumnOfFour, formulaForColumnOfThree } from "./utils/formulas";
+import { isColumnOfFour, isColumnOfThree } from "./utils/move-check-logic";
 
 function App() {
 
@@ -20,13 +22,27 @@ function App() {
     dispatch( updateBoard(createBoard(boardSize)));
   }, [dispatch, boardSize])
 
+  useEffect( () => {
+    const timeout = setTimeout( () => {
+      const newBoard = [...board];
+      isColumnOfFour(newBoard, boardSize, formulaForColumnOfFour(boardSize));
+      isColumnOfThree(newBoard, boardSize, formulaForColumnOfThree(boardSize));
+      dispatch(updateBoard(newBoard))
+      
+    }, 150);
+    return () => clearInterval(timeout)
+  },[board, dispatch, boardSize])
+
 
 
   return (
     <div className="flex items-center justify-center h-screen">
+      <h1 className=" text-7xl text-purple-200 font-black absolute top-[10%]">Candy Crush</h1>
       <Board/>
     </div>
   )
 }
+
+
 
 export default App
---
+++ src/utils/formulas.ts
@@ -0,0 +1,6 @@
+export const formulaForColumnOfFour = (boardSize: number) => 
+    boardSize * boardSize - (boardSize + boardSize + boardSize ) - 1;
+
+export const formulaForColumnOfThree = (boardSize: number) => 
+    boardSize * boardSize - (boardSize + boardSize) - 1;
+
